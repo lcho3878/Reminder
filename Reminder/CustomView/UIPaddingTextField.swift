e734c44684f8df9c5338595e4d814cd2cc50ea78
lcho3878 <leech3878@naver.com>
2024-07-02 20:26:47 +0900

    Add: UIPaddingTextField 생성

---
+++ Reminder/CustomView/UIPaddingTextField.swift
@@ -0,0 +1,31 @@
+//
+//  UIPaddingTextField.swift
+//  Reminder
+//
+//  Created by 이찬호 on 7/2/24.
+//
+
+import UIKit
+
+class UIPaddingTextField: UITextField {
+    
+    var insetX: CGFloat = 0 {
+        didSet {
+            layoutIfNeeded()
+        }
+    }
+    var insetY: CGFloat = 0 {
+        didSet {
+            layoutIfNeeded()
+        }
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, insetX, insetY)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return CGRectInset(bounds, insetX, insetY)
+    }
+    
+}
---
+++ Reminder/RegisterViewController.swift
@@ -0,0 +1,8 @@
+//
+//  RegisterViewController.swift
+//  Reminder
+//
+//  Created by 이찬호 on 7/2/24.
+//
+
+import Foundation
