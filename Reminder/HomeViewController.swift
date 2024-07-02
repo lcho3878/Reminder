2b8a19095328a0abe3199c80cef504b0e8534e03
lcho3878 <leech3878@naver.com>
2024-07-02 19:48:35 +0900

    Feat: HomeViewController 임시 UI 작성

--- Reminder/HomeViewController.swift
+++ Reminder/HomeViewController.swift
@@ -6,12 +6,69 @@
 //
 
 import UIKit
+import SnapKit
 
-class HomeViewController: BaseViewController {
-
+final class HomeViewController: BaseViewController {
+    
+    private let searchBar = {
+        let view = UISearchBar()
+        view.searchBarStyle = .minimal
+        return view
+    }()
+    
+    private let tempButton = {
+        var configuration = UIButton.Configuration.filled()
+        configuration.title = "임시버튼"
+        let bt = UIButton(configuration: configuration)
+        return bt
+    }()
+    
+    private let newTaskButton = {
+        var configuration = UIButton.Configuration.borderless()
+        configuration.title = "새로운 미리 알림"
+        configuration.baseForegroundColor = .systemBlue
+        configuration.image = UIImage(systemName: "plus.circle.fill")
+        configuration.imagePadding = 8
+        let bt = UIButton(configuration: configuration)
+        return bt
+    }()
+    
+    private let newListButton = {
+        var configuration = UIButton.Configuration.borderless()
+        configuration.title = "목록 추가"
+        configuration.baseForegroundColor = .systemBlue
+        let bt = UIButton(configuration: configuration)
+        return bt
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    override func configureHierarhy() {
+        view.addSubview(searchBar)
+        view.addSubview(tempButton)
+        view.addSubview(newTaskButton)
+        view.addSubview(newListButton)
+    }
+    
+    override func configureLayout() {
+        searchBar.snp.makeConstraints {
+            $0.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(44)
+        }
+        
+        tempButton.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        
+        newTaskButton.snp.makeConstraints {
+            $0.leading.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        newListButton.snp.makeConstraints {
+            $0.trailing.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
     }
 
 
